7f138f795af8e9640ac4c0bd9d5bcfa99ff661de
juliang2127 <164977539+juliang2127@users.noreply.github.com>
2024-04-29 06:23:42 +0000

    directors mostly done added

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -12,6 +12,9 @@ class DirectorsController < ApplicationController
   end
 
   def all_directors_details
+    the_id = params.fetch("directors_details")
+    matching_records = Director.where({ :id => the_id})
+    @the_director = matching_records.at(0)
     render({ :template => "misc_templates/directors_details"})
   end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,9 @@ class MoviesController < ApplicationController
   end
 
   def all_movies_details
+    the_id = params.fetch("all_movies_details")
+    matching_records = Movie.where({ :id => the_id})
+    @the_director = matching_records.at(0)
     render({ :template => "misc_templates/movies_details"})
   end
 end
